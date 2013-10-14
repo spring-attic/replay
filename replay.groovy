832f21d814e69dc06455d9adc75482f8d85728d8
Greg Turnquist <gturnquist@gopivotal.com>
2013-10-14 12:57:22 -0400

    Implement replay functionality to monitor "cf logs"
    Using the logs-cf-plugin, this app reads a constant stream of new log
    entries. This application parses the content, filters out invalid log
    entries, and then replays them against a different space.
    
    TODO: Add ability to figure out which one app/instance is active,
    and retrieve appropriately.

--- replay.groovy
+++ replay.groovy
@@ -1,10 +1,11 @@
 package io.spring.replay
 
 @Grab("org.springframework.boot:spring-boot-starter:0.5.0.M4")
-@Grab("org.springframework:spring-web:3.2.2.RELEASE") // this version support CF library
-@Grab("org.cloudfoundry:cloudfoundry-client-lib:0.8.7")
-@Grab("org.codehaus.jackson:jackson-core-asl:1.6.2")
-@Grab("org.codehaus.jackson:jackson-mapper-asl:1.6.2")
+
+// This version supports CF library
+// Don't know yet whether we are using vcap-java-client, but will hold
+// at this version until determined otherwise
+@Grab("org.springframework:spring-web:3.2.2.RELEASE")
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.http.client.ClientHttpResponse
@@ -12,15 +13,15 @@ import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.ResponseErrorHandler
 
-import org.cloudfoundry.client.lib.CloudCredentials
-import org.cloudfoundry.client.lib.CloudFoundryClient
-
 /**
  * Run it with "spring run replay.groovy".
  *
  * To work, you must create an application.properties file with:
  * username=<your CF username>
  * password=<your CF password>
+ *
+ * This requires that you have cf CLI installed and are already logged in
+ * and that you also have logs-cf-plugin 0.0.44.pre installed.
  *
  * NOTE: This class won't be part of the test suite
  */
@@ -30,24 +31,36 @@ class Application implements CommandLineRunner {
 	@Autowired
 	Replay replay
 
+	@Autowired
+	LogParser parser
+
 	@Value('${username}')
 	private String username
 
 	@Value('${password}')
 	private String password
 
+	/**
+	 * This application assumes you are already logged into CF
+	 * @param args - command line parameters
+	 */
 	void run(String... args) {
-		log.info "Logging in as " + username
-		def client = replay.cf(username, password)
-		client.login()
-		def logs = client.getLogs("sagan-blue")
-		logs["logs/stdout.log"].eachLine {
-			log.info it
+		log.info "Fetching logs"
+
+		def proc = "cf logs sagan-blue".execute()
+
+		proc.in.newReader().eachLine {
+			def results = parser.parse(it)
+			if (results != "Invalid log line") {
+				replay.method(results, "staging")
+			}
+
 		}
 	}
 }
 
 @Component
+@Log
 class Replay {
 
 	RestTemplate restTemplate = new RestTemplate()
@@ -68,38 +81,25 @@ class Replay {
 
 	def method(Line arg, String space = null) {
 		if (space) {
-			"http://${space}.${arg.host}${arg.path}".toString()
+			call(arg, "http://${space}.${arg.host}${arg.path}")
 		} else {
 			call(arg, "http://${arg.host}${arg.path}")
 		}
 	}
 
-	def call(Line arg, String query) {
+	private def call(Line arg, String query) {
 		if (arg.method == "GET") {
 			try {
+				log.info "Invoking GET ${query}"
 				restTemplate.getForObject(query, String.class)
 			} catch (HttpClientErrorException e) {
 				// Absorb 404 errors
 			}
 			true
 		} else {
-			//println "We don't handle ${arg.method}"
+			log.info "We don't handle ${arg.method}"
 			false
 		}
-	}
-
-	def cf(String username, String password) {
-		URL target = "https://api.run.pivotal.io".toURL()
-
-		/**
-		 * Plugin your own credentials
-		 * P.S. "spring test" doesn't create an app context, and hence doesn't (yet) support
-		 * properties
-		 */
-		//CloudCredentials credentials = new CloudCredentials("", "")
-		CloudCredentials credentials = new CloudCredentials(username, password)
-		CloudFoundryClient client = new CloudFoundryClient(credentials, target, "spring.io", "production")
-		client
 	}
 
 }
@@ -114,13 +114,14 @@ class Line {
 	}
 }
 
+@Service
 class LogParser {
 	def parse(String line) {
 		def preprocessed = line.split()
-		if (preprocessed.length > 3 && preprocessed[3] == "spring.io") {
-			new Line(host: preprocessed[3],
-					path: preprocessed[8],
-					method: preprocessed[7][1..-1]
+		if (preprocessed.length >= 9) {
+			new Line(host: preprocessed[6],
+					path: preprocessed[11],
+					method: preprocessed[10][1..-1]
 			)
 		} else {
 			"Invalid log line"
--- test.groovy
+++ test.groovy
@@ -13,16 +13,16 @@ class LogParserTests {
 
 	@Test
 	void parseLineNotGoingToMainWebsite() {
-		String data = "sagan-blue CF[Router]  STDOUT sagan-production.cfapps.io - [10/10/2013:15:52:31 +0000] \"GET /blog/2011/11/24/spring-roo-1-2-0-rc1-released HTTP/1.1\" 200 15002 \"-\" \"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)\" 10.10.66.79:14835 response_time:0.022856244 app_id:88f6663b-53b5-4d64-afab-837e4b5a6828"
+		String data = "Connected to server."
 		assertEquals("Invalid log line", parser.parse(data))
 	}
 
 	@Test
 	void parseLineHittingMainWebsite() {
-		String data = "sagan-blue CF[Router]  STDOUT spring.io - [10/10/2013:15:42:57 +0000] \"GET /blog/2010/01/25/ajax-simplifications-in-spring-3-0/null?s=20&d=mm HTTP/1.1\" 404 9301 \"http://spring.io/blog/2010/01/25/ajax-simplifications-in-spring-3-0/\" \"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0\" 10.10.2.31:61475 response_time:0.017503378 app_id:88f6663b-53b5-4d64-afab-837e4b5a6828"
+		String data = "Oct 14 12:41:02.734 sagan-blue CF[Router] STDOUT spring.io - [14/10/2013:16:41:02 +0000] \"GET /blog/2009/03/08/rest-in-spring-3-mvc/ HTTP/1.1\" 200 31164 \"http://www.bing.com/search?q=Spring+mvc+rest+with+nested+variables&qs=n&form=QBRE&pq=spring+mvc+rest+with+nested+variables&sc=0-16&sp=-1&sk=&cvid=a0c07c59ffcc4e7b8a6e2ac6a04fa989\" \"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)\" 10.10.2.159:49646 response_time:0.023646007 app_id:dba30dd4-e349-4aa4-a9fd-92649e8b3db5"
 		def results = parser.parse(data)
 		assertEquals("GET", results.method)
-		assertEquals("/blog/2010/01/25/ajax-simplifications-in-spring-3-0/null?s=20&d=mm", results.path)
+		assertEquals("/blog/2009/03/08/rest-in-spring-3-mvc/", results.path)
 	}
 
 }
@@ -52,51 +52,6 @@ class ReplayTests {
 	@Ignore
 	void testHeadMethod() {
 		fail("You need to code a HEAD test")
-	}
-
-}
-
-class IntegrationTests {
-
-	LogParser parser
-	Replay replay
-
-	@Before
-	void setup() {
-		parser = new LogParser()
-		replay = new Replay()
-	}
-
-	@Test
-	@Ignore
-	void testTheBacklogWithoutAnyErrors() {
-		new File("access-prod.log").eachLine { line ->
-			def results = parser.parse(line)
-			if (results != "Invalid log line") {
-				replay.method(results)
-			}
-		}
-	}
-
-}
-
-class CloudFoundryTests {
-
-	Replay replay
-
-	@Before
-	void setup() {
-		replay = new Replay()
-	}
-
-	@Test
-	void cf() {
-		def client = replay.cf("replace with your CF username", "replace with your CF password")
-		println client
-		client.login()
-		def logs = client.getLogs("sagan-blue")
-		logs.each { println it.key }
-//		println logs["logs/stdout.log"]
 	}
 
 }
